2499f3f26c78d0caec0e94de06c7fffaa023d4e7
mdriduanislam <mohhammadris786@gmail.com>
2026-05-30 23:22:04 +0600

    solved characterCounter

--- 10_characterCounter.js
+++ 10_characterCounter.js
@@ -1,10 +1,17 @@
 function characterCounter(str,char){
     let count = 0;
     for(let i=0; i<str.length; i++){
-        if(str[i]==char){
+        if(str[i].toLowerCase()==char.toLowerCase()){
             count++;
         }
     }
     return count;
 }
-console.log(characterCounter('tuttu','t'))
+console.log(characterCounter('tuttu','T'))
+
+function countChar(str, char) 
+{
+  return str.toLowerCase().split(char.toLowerCase()).length - 1;
+}
+
+console.log(countChar('GeeksForGeeks', 'e'));
