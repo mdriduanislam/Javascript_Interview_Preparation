703f973cf2e9b3551b0d1408fe281593276ca96f
mdriduanislam <mohhammadris786@gmail.com>
2026-06-01 23:24:04 +0600

    solved if a string contains substring

---
+++ 14_containsSubString.js
@@ -0,0 +1,6 @@
+function containsSubstring(str, substring) {
+//searches for the substring within str
+return str.indexOf(substring) > 0;
+}
+
+console.log(containsSubstring('GeeksForGeeks', 'to'));
